baa95df48f46d56141c34a628ed9510eaf1d7342
Caraway <m-begale@northwestern.edu>
2016-02-21 17:34:00 -0600

    updated charts and calling code

--- platforms/browser/www/scripts/controllers/charts.js
+++ platforms/browser/www/scripts/controllers/charts.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 /**
@@ -9,7 +8,7 @@
  * Controller of the livewellApp
  */
 angular.module('livewellApp')
-  .controller('ChartsCtrl', function ($scope,Pound) {
+    .controller('ChartsCtrl', function($scope, Pound) {
 
         $scope.pageTitle = 'My Charts';
 
@@ -21,16 +20,15 @@ angular.module('livewellApp')
         $scope.recodedResponses = JSON.parse(localStorage['recodedResponses']);
 
         $scope.graph = []
-        if ($scope.dailyCheckInResponseArray.length > 7){
-            $scope.graph[6] = $scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length-1];
-            $scope.graph[5] = $scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length-2];
-            $scope.graph[4] = $scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length-3];
-            $scope.graph[3] = $scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length-4];
-            $scope.graph[2] = $scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length-5];
-            $scope.graph[1] = $scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length-6];
-            $scope.graph[0] = $scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length-7];
-        }
-        else{
+        if ($scope.dailyCheckInResponseArray.length > 7) {
+            $scope.graph[6] = $scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length - 1];
+            $scope.graph[5] = $scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length - 2];
+            $scope.graph[4] = $scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length - 3];
+            $scope.graph[3] = $scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length - 4];
+            $scope.graph[2] = $scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length - 5];
+            $scope.graph[1] = $scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length - 6];
+            $scope.graph[0] = $scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length - 7];
+        } else {
             $scope.graph = $scope.dailyCheckInResponseArray;
         }
 
@@ -45,12 +43,12 @@ angular.module('livewellApp')
         }
 
         $scope.routine = {
-            class: function(value){
-              debugger;
+            class: function(value) {
+                debugger;
                 var returnvalue = null;
-                switch(value) {
+                switch (value) {
                     case 0:
-                        returnvalue ='a';
+                        returnvalue = 'a';
                         break;
                     case 1:
                         returnvalue = 'b';
@@ -64,11 +62,11 @@ angular.module('livewellApp')
         };
 
         $scope.medication = {
-            class: function(value){
+            class: function(value) {
                 var returnvalue = null;
-                switch(value) {
+                switch (value) {
                     case 0:
-                        returnvalue ='a';
+                        returnvalue = 'a';
                         break;
                     case 0.5:
                         returnvalue = 'b';
@@ -82,11 +80,11 @@ angular.module('livewellApp')
         };
 
         $scope.sleep = {
-            class: function(value){
+            class: function(value) {
                 var returnvalue = null;
-                switch(value) {
+                switch (value) {
                     case (value < 0):
-                        returnvalue ='a';
+                        returnvalue = 'a';
                         break;
                     case (value == 0):
                         returnvalue = 'b';
@@ -102,240 +100,243 @@ angular.module('livewellApp')
 
 
 
-        Highcharts.theme =  {colors: ["#2b908f", "#90ee7e", "#f45b5b", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
-      "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
-   chart: {
-      backgroundColor: 'transparent',
-      style: {
-         fontFamily: "sans-serif"
-      },
-      plotBorderColor: '#606063'
-   },
-   title: {
-      style: {
-         color: '#E0E0E3',
-         textTransform: 'uppercase',
-         fontSize: '20px'
-      }
-   },
-   subtitle: {
-      style: {
-         color: '#E0E0E3',
-         textTransform: 'uppercase'
-      }
-   },
-   xAxis: {
-      gridLineColor: '#707073',
-      labels: {
-         style: {
-            color: '#E0E0E3'
-         }
-      },
-      lineColor: '#FFFFFF',
-      minorGridLineColor: '#505053',
-      tickColor: '#707073',
-      title: {
-         style: {
-            color: '#A0A0A3'
-
-         }
-      }
-   },
-   yAxis: {
-      gridLineColor: '#707073',
-      labels: {
-         style: {
-            color: '#E0E0E3'
-         }
-      },
-      lineColor: '#707073',
-      minorGridLineColor: '#505053',
-      tickColor: '#707073',
-      tickWidth: 1,
-      title: {
-         style: {
-            color: '#A0A0A3'
-         }
-      }
-   },
-   tooltip: {
-      backgroundColor: 'rgba(0, 0, 0, 0.85)',
-      style: {
-         color: '#F0F0F0'
-      }
-   },
-   plotOptions: {
-      series: {
-         dataLabels: {
-            color: '#B0B0B3'
-         },
-         marker: {
-            lineColor: 'white'
-         }
-      },
-      boxplot: {
-         fillColor: '#505053'
-      },
-      candlestick: {
-         lineColor: 'white'
-      },
-      errorbar: {
-         color: 'white'
-      }
-   },
-   legend: {
-      itemStyle: {
-         color: '#E0E0E3'
-      },
-      itemHoverStyle: {
-         color: '#FFF'
-      },
-      itemHiddenStyle: {
-         color: '#606063'
-      }
-   },
-   credits: {
-      style: {
-         color: '#666'
-      }
-   },
-   labels: {
-      style: {
-         color: '#707073'
-      }
-   },
-
-   drilldown: {
-      activeAxisLabelStyle: {
-         color: '#F0F0F3'
-      },
-      activeDataLabelStyle: {
-         color: '#F0F0F3'
-      }
-   },
-
-   navigation: {
-      buttonOptions: {
-         symbolStroke: '#DDDDDD',
-         theme: {
-            fill: '#505053'
-         }
-      }
-   },
-
-   // scroll charts
-   rangeSelector: {
-      buttonTheme: {
-         fill: '#505053',
-         stroke: '#000000',
-         style: {
-            color: '#CCC'
-         },
-         states: {
-            hover: {
-               fill: '#707073',
-               stroke: '#000000',
-               style: {
-                  color: 'white'
-               }
+        Highcharts.theme = {
+            chart: {
+                backgroundColor: 'transparent',
+                style: {
+                    fontFamily: "sans-serif"
+                },
+                plotBorderColor: '#606063'
             },
-            select: {
-               fill: '#000003',
-               stroke: '#000000',
-               style: {
-                  color: 'white'
-               }
-            }
-         }
-      },
-      inputBoxBorderColor: '#505053',
-      inputStyle: {
-         backgroundColor: '#333',
-         color: 'silver'
-      },
-      labelStyle: {
-         color: 'silver'
-      }
-   },
-
-   navigator: {
-      handles: {
-         backgroundColor: '#666',
-         borderColor: '#AAA'
-      },
-      outlineColor: '#CCC',
-      maskFill: 'rgba(255,255,255,0.1)',
-      series: {
-         color: '#7798BF',
-         lineColor: 'white'
-      },
-      xAxis: {
-         gridLineColor: '#505053'
-      }
-   },
-
-   scrollbar: {
-      barBackgroundColor: '#808083',
-      barBorderColor: '#808083',
-      buttonArrowColor: '#CCC',
-      buttonBackgroundColor: '#606063',
-      buttonBorderColor: '#606063',
-      rifleColor: '#FFF',
-      trackBackgroundColor: '#404043',
-      trackBorderColor: '#404043'
-   },
-
-   // special colors for some of the
-   legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
-   background2: '#505053',
-   dataLabelsColor: '#B0B0B3',
-   textColor: '#C0C0C0',
-   contrastTextColor: '#F0F0F3',
-   maskColor: 'rgba(255,255,255,0.3)'
-};
-
-Highcharts.setOptions(Highcharts.theme);
-
-  $scope.config = {
-         title: {
-            text: ' ',
-            x: -20, //center,
-            enabled:false
-        },
-        subtitle: {
-            text: '',
-            x: -20
-        },
-        xAxis: {
-            categories:  $scope.dates
-        },
-        yAxis: {
             title: {
-                text: ''
+                style: {
+                    color: '#E0E0E3',
+                    textTransform: 'uppercase',
+                    fontSize: '20px'
+                }
             },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }],
-            labels:{
-                enabled:false
-            }
-        },
-        tooltip: {
-            valueSuffix: '',
-            enabled:false
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle',
-            borderWidth: 0,
-            enabled:false
-        },
-        series: [{
-             showInLegend: false,
-            name: 'Wellness',
-            data: $scope.wellness
-        }]};
-  });
+            subtitle: {
+                style: {
+                    color: '#E0E0E3',
+                    textTransform: 'uppercase'
+                }
+            },
+            xAxis: {
+                gridLineColor: '#707073',
+                labels: {
+                    style: {
+                        color: '#E0E0E3'
+                    }
+                },
+                lineColor: '#FFFFFF',
+                minorGridLineColor: '#505053',
+                tickColor: '#707073',
+                title: {
+                    style: {
+                        color: '#A0A0A3'
+
+                    }
+                }
+            },
+            yAxis: {
+                gridLineColor: '#707073',
+                labels: {
+                    style: {
+                        color: '#E0E0E3'
+                    }
+                },
+                lineColor: 'white',
+                minorGridLineColor: '#505053',
+                tickColor: '#707073',
+                tickWidth: 1,
+                title: {
+                    style: {
+                        color: '#A0A0A3'
+                    }
+                }
+            },
+            tooltip: {
+                backgroundColor: 'rgba(0, 0, 0, 0.85)',
+                style: {
+                    color: '#F0F0F0'
+                }
+            },
+            plotOptions: {
+                series: {
+                    dataLabels: {
+                        color: '#B0B0B3'
+                    },
+                    marker: {
+                        lineColor: 'white'
+                    }
+                },
+                boxplot: {
+                    fillColor: '#505053'
+                },
+                candlestick: {
+                    lineColor: 'white'
+                },
+                errorbar: {
+                    color: 'white'
+                }
+            },
+            legend: {
+                itemStyle: {
+                    color: '#E0E0E3'
+                },
+                itemHoverStyle: {
+                    color: '#FFF'
+                },
+                itemHiddenStyle: {
+                    color: '#606063'
+                }
+            },
+            credits: {
+                style: {
+                    color: '#666'
+                }
+            },
+            labels: {
+                style: {
+                    color: '#707073'
+                }
+            },
+
+            drilldown: {
+                activeAxisLabelStyle: {
+                    color: '#F0F0F3'
+                },
+                activeDataLabelStyle: {
+                    color: '#F0F0F3'
+                }
+            },
+
+            navigation: {
+                buttonOptions: {
+                    symbolStroke: '#DDDDDD',
+                    theme: {
+                        fill: '#505053'
+                    }
+                }
+            },
+
+            // scroll charts
+            rangeSelector: {
+                buttonTheme: {
+                    fill: '#505053',
+                    stroke: '#000000',
+                    style: {
+                        color: '#CCC'
+                    },
+                    states: {
+                        hover: {
+                            fill: '#707073',
+                            stroke: '#000000',
+                            style: {
+                                color: 'white'
+                            }
+                        },
+                        select: {
+                            fill: '#000003',
+                            stroke: '#000000',
+                            style: {
+                                color: 'white'
+                            }
+                        }
+                    }
+                },
+                inputBoxBorderColor: '#505053',
+                inputStyle: {
+                    backgroundColor: '#333',
+                    color: 'silver'
+                },
+                labelStyle: {
+                    color: 'silver'
+                }
+            },
+
+            navigator: {
+                handles: {
+                    backgroundColor: '#666',
+                    borderColor: '#AAA'
+                },
+                outlineColor: '#CCC',
+                maskFill: 'rgba(255,255,255,0.1)',
+                series: {
+                    color: '#7798BF',
+                    lineColor: 'white'
+                },
+                xAxis: {
+                    gridLineColor: '#505053'
+                }
+            },
+
+            scrollbar: {
+                barBackgroundColor: '#808083',
+                barBorderColor: '#808083',
+                buttonArrowColor: '#CCC',
+                buttonBackgroundColor: '#606063',
+                buttonBorderColor: '#606063',
+                rifleColor: '#FFF',
+                trackBackgroundColor: '#404043',
+                trackBorderColor: '#404043'
+            },
+
+            // special colors for some of the
+            legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
+            background2: '#505053',
+            dataLabelsColor: '#B0B0B3',
+            textColor: '#C0C0C0',
+            contrastTextColor: '#F0F0F3',
+            maskColor: 'rgba(255,255,255,0.3)'
+        };
+
+        Highcharts.setOptions(Highcharts.theme);
+
+        $scope.config = {
+            title: {
+                text: ' ',
+                x: -20, //center,
+                enabled: false
+            },
+            subtitle: {
+                text: '',
+                x: -20
+            },
+            xAxis: {
+                categories: $scope.dates
+            },
+            yAxis: {
+                title: {
+                    text: ''
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: 'white'
+                }],
+                labels: {
+                    enabled: false
+                }
+            },
+            tooltip: {
+                valueSuffix: '',
+                enabled: false
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0,
+                enabled: false
+            },
+            exporting: {
+                 enabled: false 
+            },
+            series: [{
+                showInLegend: false,
+                name: 'Wellness',
+                data: $scope.wellness
+            }]
+        };
+    });
--- platforms/browser/www/scripts/controllers/daily_review.js
+++ platforms/browser/www/scripts/controllers/daily_review.js
@@ -8,81 +8,111 @@
  * Controller of the livewellApp
  */
 angular.module('livewellApp')
-  .controller('DailyReviewCtrl', function ($scope,$routeParams,UserData,Pound,DailyReviewAlgorithm,ClinicalStatusUpdate,Guid) {
-    $scope.pageTitle = "Daily Review";
-    Pound.add('dailyReviewStarted',{userStarted:true, code:$scope.code});
+    .controller('DailyReviewCtrl', function($scope, $routeParams, UserData, Pound, DailyReviewAlgorithm, ClinicalStatusUpdate, Guid) {
+        $scope.pageTitle = "Daily Review";
+        
+        Pound.add('dailyReviewStarted', {
+            userStarted: true,
+            code: $scope.code
+        });
 
-    $scope.interventionGroups = UserData.query('dailyReview');
+        $scope.interventionGroups = UserData.query('dailyReview');
 
-    $scope.updatedClinicalStatus = {};
+        $scope.updatedClinicalStatus = {};
 
-    var runAlgorithm = function(Pound){
-    	var object = {};
-        var sessionID = Guid.create();
-    
-    	object.code = DailyReviewAlgorithm.code();
-    	$scope.updatedClinicalStatus = ClinicalStatusUpdate.execute();
+        var runAlgorithm = function(Pound) {
+            var object = {};
+            var sessionID = Guid.create();
 
-        (new PurpleRobot()).emitReading('livewell_dailyreviewcode',{sessionGUID: sessionID, code:object.code}).execute();
-        (new PurpleRobot()).emitReading('livewell_clinicalstatus',{sessionGUID: sessionID, status:$scope.updatedClinicalStatus}).execute();
+            object.code = DailyReviewAlgorithm.code();
+            $scope.updatedClinicalStatus = ClinicalStatusUpdate.execute();
 
-    	return object
+            (new PurpleRobot()).emitReading('livewell_dailyreviewcode', {
+                sessionGUID: sessionID,
+                code: object.code
+            }).execute();
+            (new PurpleRobot()).emitReading('livewell_clinicalstatus', {
+                sessionGUID: sessionID,
+                status: $scope.updatedClinicalStatus
+            }).execute();
 
-    }
+            return object
 
-    $scope.code = runAlgorithm().code;
-   
-    //TO REMOVE
-    $scope.recodedResponses = DailyReviewAlgorithm.recodedResponses();
-    $scope.dailyCheckInResponseArray = Pound.find('dailyCheckIn')
-    $scope.dailyCheckInResponses = ' |today| ' +JSON.stringify($scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length-1]) +' |t-1| ' +JSON.stringify($scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length-2]) + ' |t-2| ' + JSON.stringify($scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length-3])+ ' |t-3| ' + JSON.stringify($scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length-4]) + ' |t-4| ' + JSON.stringify($scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length-5])+ ' |t-5| ' + JSON.stringify($scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length-6])+ ' |t-6| ' + JSON.stringify($scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length-7]);
-
-		//STOP REMOVE
-
-    $scope.percentages = DailyReviewAlgorithm.percentages();
-
-    $(".modal-backdrop").remove();
-
-    var latestWarning = Pound.find('clinical_reachout')[Pound.find('clinical_reachout').length-1];
-
-    if (latestWarning != undefined){
-        if (latestWarning.shownToUser == undefined){
-            $scope.warningMessage = Pound.find('clinical_reachout')[Pound.find('clinical_reachout').length-1].message
-            $scope.psychiatristEmail = _.where(JSON.parse(localStorage.team),{role:'Psychiatrist'})[0].email;
-
-            if (_.where(JSON.parse(localStorage.team),{role:'Coach'})[0] != undefined){
-            $scope.coachEmail = _.where(JSON.parse(localStorage.team),{role:'Coach'})[0].email;
-            } else{ $scope.coachEmail = ''}
-
-            (new PurpleRobot()).emitReading('livewell_email',{psychiatristEmail:$scope.psychiatristEmail, coachEmail: $scope.coachEmail, message: $scope.warningMessage}).execute();
-
-            latestWarning.shownToUser = true;
-            Pound.update('clinical_reachout',latestWarning);
-            $scope.showWarning = true;
-            $("#warning").modal();
         }
-    }
+
+        $scope.code = runAlgorithm().code;
+
+        //TO REMOVE
+        $scope.recodedResponses = DailyReviewAlgorithm.recodedResponses();
+        $scope.dailyCheckInResponseArray = Pound.find('dailyCheckIn')
+        $scope.dailyCheckInResponses = ' |today| ' + JSON.stringify($scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length - 1]) + ' |t-1| ' + JSON.stringify($scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length - 2]) + ' |t-2| ' + JSON.stringify($scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length - 3]) + ' |t-3| ' + JSON.stringify($scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length - 4]) + ' |t-4| ' + JSON.stringify($scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length - 5]) + ' |t-5| ' + JSON.stringify($scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length - 6]) + ' |t-6| ' + JSON.stringify($scope.dailyCheckInResponseArray[$scope.dailyCheckInResponseArray.length - 7]);
+
+        //STOP REMOVE
+
+        $scope.percentages = DailyReviewAlgorithm.percentages();
+
+        $(".modal-backdrop").remove();
+
+        var latestWarning = Pound.find('clinical_reachout')[Pound.find('clinical_reachout').length - 1];
+
+        if (latestWarning != undefined) {
+            if (latestWarning.shownToUser == undefined) {
+                $scope.warningMessage = Pound.find('clinical_reachout')[Pound.find('clinical_reachout').length - 1].message
+                $scope.psychiatristEmail = _.where(JSON.parse(localStorage.team), {
+                    role: 'Psychiatrist'
+                })[0].email;
+
+                if (_.where(JSON.parse(localStorage.team), {
+                        role: 'Coach'
+                    })[0] != undefined) {
+                    $scope.coachEmail = _.where(JSON.parse(localStorage.team), {
+                        role: 'Coach'
+                    })[0].email;
+                } else {
+                    $scope.coachEmail = ''
+                }
+
+                (new PurpleRobot()).emitReading('livewell_email', {
+                    psychiatristEmail: $scope.psychiatristEmail,
+                    coachEmail: $scope.coachEmail,
+                    message: $scope.warningMessage
+                }).execute();
+
+                latestWarning.shownToUser = true;
+                Pound.update('clinical_reachout', latestWarning);
+                $scope.showWarning = true;
+                $("#warning").modal();
+            }
+        }
+
+        $scope.dailyReviewCategory = _.where($scope.interventionGroups, {
+            code: $scope.code
+        })[0].questionSet;
+
+        $scope.interventionResponse = function() {
+
+            if ($scope.code == 1 || $scope.code == 2) {
+                return 'Please contact your care provider or hospital';
+            } else {
+                if (_.where($scope.interventionGroups, {
+                        code: $scope.code
+                    })[0] != undefined) {
+                    if (typeof(_.where($scope.interventionGroups, {
+                            code: $scope.code
+                        })[0].response) == 'object') {
+                        return _.where($scope.interventionGroups, {
+                            code: $scope.code
+                        })[0].response[Math.floor((Math.random() * _.where($scope.interventionGroups, {
+                            code: $scope.code
+                        })[0].response.length))]
+                    } else {
+                        return _.where($scope.interventionGroups, {
+                            code: $scope.code
+                        })[0].response
+                    }
+                }
+            }
+        }
 
 
-
-    $scope.dailyReviewCategory = _.where($scope.interventionGroups, {code:$scope.code})[0].questionSet;
-
-		$scope.interventionResponse = function(){ 
-
-			if ($scope.code == 1 || $scope.code == 2 ){ 
-					return 'Please contact your care provider or hospital';
-			}
-			else {
-				if (_.where($scope.interventionGroups, {code:$scope.code})[0] != undefined){
-					if(typeof(_.where($scope.interventionGroups, {code:$scope.code})[0].response) == 'object'){
-							return _.where($scope.interventionGroups, {code:$scope.code})[0].response[Math.floor((Math.random() * _.where($scope.interventionGroups, {code:$scope.code})[0].response.length)	)]
-					}
-					else {
-							return _.where($scope.interventionGroups, {code:$scope.code})[0].response
-					}
-				}
-			}
-		}
-
-
-  });
+    });
